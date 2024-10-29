b2eb8b1f582d2fa7c8bd19c48ef8f27048d22f44
Douglas <doug104397@gmail.com>
2024-10-29 00:07:38 -0300

    ajuste na apresentação das informações do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,136 +1,121 @@
 import React from "react";
-import { Box, Typography, Grid, Link } from "@mui/material";
+import { Box, Typography, Link } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 
 const FooterContainer = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.grayscale.darkGray,
-  color: theme.palette.grayscale.white,
-  padding: "2em 0",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
+  backgroundColor: "#1A2027",
+  color: "#fff",
+  padding: theme.spacing(4, 0),
 }));
 
-const FooterContent = styled(Grid)(({ theme }) => ({
+const FooterContent = styled(Box)({
   width: "80%",
-  maxWidth: "1200px",
+  maxWidth: "100%",
   margin: "0 auto",
-}));
+  display: "grid",
+  gridTemplateColumns: "repeat(4, 1fr)",
+  gap: "2rem",
+});
 
 const FooterColumn = styled(Box)({
   display: "flex",
   flexDirection: "column",
-  gap: "0.8em",
+  gap: "0.8rem",
+  wordBreak: "break-word",
+  overflowWrap: "break-word",
 });
 
 const FooterTitle = styled(Typography)({
   fontWeight: "bold",
-  marginBottom: "1em",
+  marginBottom: "1rem",
+  color: "#fff",
 });
 
 const SocialIcons = styled(Box)({
   display: "flex",
-  gap: "1em",
-  marginTop: "1em",
+  gap: "1rem",
+  marginTop: "1rem",
+  "& svg": {
+    color: "#fff",
+  },
 });
 
-const Copyright = styled(Typography)(({ theme }) => ({
-  marginTop: "2em",
-  fontSize: "0.9em",
-  borderTop: `1px solid ${theme.palette.grayscale.lightGray3}`,
-  paddingTop: "1em",
-}));
+const StyledLink = styled(Link)({
+  color: "#fff",
+  textDecoration: "none",
+  "&:hover": {
+    textDecoration: "underline",
+  },
+});
+
+const Copyright = styled(Typography)({
+  marginTop: "2rem",
+  paddingTop: "1rem",
+  borderTop: "1px solid rgba(255, 255, 255, 0.1)",
+  textAlign: "center",
+  color: "#fff",
+});
 
 function Footer() {
   return (
     <FooterContainer>
-      <FooterContent container spacing={4}>
-        <Grid item xs={12} sm={3}>
-          <FooterColumn>
-            <Typography variant="h6" component="div">
-              <strong>Digital Store</strong>
-            </Typography>
-            <Typography variant="body2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore.
-            </Typography>
-            <SocialIcons>
-              <Link href="#" color="inherit" aria-label="Facebook">
-                <FacebookIcon />
-              </Link>
-              <Link href="#" color="inherit" aria-label="Instagram">
-                <InstagramIcon />
-              </Link>
-              <Link href="#" color="inherit" aria-label="Twitter">
-                <TwitterIcon />
-              </Link>
-            </SocialIcons>
-          </FooterColumn>
-        </Grid>
+      <FooterContent>
+        <FooterColumn>
+          <Typography variant="h6" component="div" sx={{ fontWeight: "bold" }}>
+            Digital Store
+          </Typography>
+          <Typography variant="body2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore.
+          </Typography>
+          <SocialIcons>
+            <StyledLink href="#" aria-label="Facebook">
+              <FacebookIcon />
+            </StyledLink>
+            <StyledLink href="#" aria-label="Instagram">
+              <InstagramIcon />
+            </StyledLink>
+            <StyledLink href="#" aria-label="Twitter">
+              <TwitterIcon />
+            </StyledLink>
+          </SocialIcons>
+        </FooterColumn>
 
-        <Grid item xs={12} sm={3}>
-          <FooterColumn>
-            <FooterTitle variant="subtitle1">Informação</FooterTitle>
-            <Link href="#" color="inherit">
-              Sobre Drip Store
-            </Link>
-            <Link href="#" color="inherit">
-              Segurança
-            </Link>
-            <Link href="#" color="inherit">
-              Wishlist
-            </Link>
-            <Link href="#" color="inherit">
-              Blog
-            </Link>
-            <Link href="#" color="inherit">
-              Trabalhe conosco
-            </Link>
-            <Link href="#" color="inherit">
-              Meus Pedidos
-            </Link>
-          </FooterColumn>
-        </Grid>
+        <FooterColumn>
+          <FooterTitle variant="subtitle1">Information</FooterTitle>
+          <StyledLink href="#">About Drip Store</StyledLink>
+          <StyledLink href="#">Security</StyledLink>
+          <StyledLink href="#">Wishlist</StyledLink>
+          <StyledLink href="#">Blog</StyledLink>
+          <StyledLink href="#">Work with Us</StyledLink>
+          <StyledLink href="#">My Orders</StyledLink>
+        </FooterColumn>
 
-        <Grid item xs={12} sm={3}>
-          <FooterColumn>
-            <FooterTitle variant="subtitle1">Categorias</FooterTitle>
-            <Link href="#" color="inherit">
-              Camisetas
-            </Link>
-            <Link href="#" color="inherit">
-              Calças
-            </Link>
-            <Link href="#" color="inherit">
-              Bonés
-            </Link>
-            <Link href="#" color="inherit">
-              Headphones
-            </Link>
-            <Link href="#" color="inherit">
-              Tênis
-            </Link>
-          </FooterColumn>
-        </Grid>
+        <FooterColumn>
+          <FooterTitle variant="subtitle1">Categories</FooterTitle>
+          <StyledLink href="#">T-shirts</StyledLink>
+          <StyledLink href="#">Pants</StyledLink>
+          <StyledLink href="#">Hats</StyledLink>
+          <StyledLink href="#">Headphones</StyledLink>
+          <StyledLink href="#">Sneakers</StyledLink>
+        </FooterColumn>
 
-        <Grid item xs={12} sm={3}>
-          <FooterColumn>
-            <FooterTitle variant="subtitle1">Contato</FooterTitle>
-            <Typography variant="body2">
-              Av. Santos Dumont, 1510 - 1 andar
-              <br />
-              Aldeota, Fortaleza - CE, 60150-161
-              <br />
-              (85) 3051-3411
-            </Typography>
-          </FooterColumn>
-        </Grid>
+        <FooterColumn>
+          <FooterTitle variant="subtitle1">Contact</FooterTitle>
+          <Typography variant="body2">
+            Av. Santos Dumont, 1510 - 1 andar
+            <br />
+            Aldeota, Fortaleza - CE, 60150-161
+            <br />
+            (85) 3051-3411
+          </Typography>
+        </FooterColumn>
       </FooterContent>
 
-      <Copyright>@ 2022 Digital College</Copyright>
+      <Copyright>© 2024 Digital College</Copyright>
     </FooterContainer>
   );
 }
